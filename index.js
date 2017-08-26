d72dab2edd0dd73a66b3866094d6286abfbcdd77
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-08-27 09:29:39 +1000

    Convert crawler into a class to hold logger

--- index.js
+++ index.js
@@ -2,104 +2,114 @@
 const chromeLauncher = require('chrome-launcher');
 const CDP = require('chrome-remote-interface');
 
-module.exports = function crawl(params) {
-    const state = {
-        settings: params.instructions
-    };
-    return Promise.resolve(state)
-        .then(prepareChrome)
-        .then(executeInstructions)
-        .then(terminateChrome)
-        .then(buildResult);
-};
+class Crawler {
 
-function prepareChrome(state) {
-    return launchChrome()
-        .then(chrome =>
-            CDP({port: chrome.port}).then(protocol => {
-                const {Page, Runtime} = protocol;
-                return Promise.all([Page.enable(), Runtime.enable()])
-                    .then(() => ({chrome, protocol, Page, Runtime}));
-            })
-        )
-        .then(chromeObjects => Object.assign({}, state, chromeObjects));
-}
-
-function launchChrome(options = {}) {
-    const width = options.width || 1440;
-    const height = options.height || 600;
-    return chromeLauncher.launch({
-        chromeFlags: [
-            `--window-size=${width},${height}`,
-            '--disable-gpu',
-            '--headless'
-        ]
-    });
-}
-
-function executeInstructions(state) {
-    const initialContext = {
-        Page: state.Page,
-        Runtime: state.Runtime,
-        instructionResults: []
-    };
-    return state.settings.instructions.reduce(
-        (promiseOfContext, instruction, index) =>
-            promiseOfContext.then(context => {
-                console.log(`> Executing instruction ${index + 1}/${state.settings.instructions.length}`);
-                return executeInstruction(instruction, context).then(result =>
-                    Object.assign({}, context, {instructionResults: [...context.instructionResults, result]})
-                );
-            }),
-        Promise.resolve(initialContext)
-    ).then(context => Object.assign({}, state, {
-        instructionContext: {instructionResults: context.instructionResults}
-    }));
-}
-
-function executeInstruction(instruction, context) {
-    const locations = typeof instruction.locations === 'function' ?
-        instruction.locations(context) :
-        instruction.locations;
-    const sequentialEvaluations = locations.reduce((promise, location, index) =>
-        promise.then(results => {
-            console.log(`>> Executing location ${index + 1}/${locations.length}`);
-            return executeExpression(location, instruction.expression, context)
-                .then(result => [...results, result])
-        }),
-        Promise.resolve([])
-    );
-    return sequentialEvaluations;
-}
-
-function executeExpression(location, expression, context) {
-    return new Promise((resolve, reject) => {
-        context.Page.navigate({url: location});
-        context.Page.loadEventFired(() => {
-            const params = {
-                expression,
-                awaitPromise: true
-            };
-            return context.Runtime.evaluate(params).then(resolve, reject);
-        });
-    })
-    .then(result => JSON.parse(extractResult(result)));
-}
-
-function extractResult(evaluationResult) {
-    if (evaluationResult.exceptionDetails) {
-        const errorString = JSON.stringify(evaluationResult.exceptionDetails, true, 2);
-        throw new Error(`Expression evaluation failed: ${errorString}`);
+    constructor(params) {
+        this._logger = params.logger;
     }
-    return evaluationResult.result.value;
+
+    crawl(params) {
+        const state = {
+            settings: params.instructions
+        };
+        return Promise.resolve(state)
+            .then(state => this._prepareChrome(state))
+            .then(state => this._executeInstructions(state))
+            .then(state => this._terminateChrome(state))
+            .then(state => this._buildResult(state));
+    }
+
+    _prepareChrome(state) {
+        return this._launchChrome()
+            .then(chrome =>
+                CDP({port: chrome.port}).then(protocol => {
+                    const {Page, Runtime} = protocol;
+                    return Promise.all([Page.enable(), Runtime.enable()])
+                        .then(() => ({chrome, protocol, Page, Runtime}));
+                })
+            )
+            .then(chromeObjects => Object.assign({}, state, chromeObjects));
+    }
+
+    _launchChrome(options = {}) {
+        const width = options.width || 1440;
+        const height = options.height || 600;
+        return chromeLauncher.launch({
+            chromeFlags: [
+                `--window-size=${width},${height}`,
+                '--disable-gpu',
+                '--headless'
+            ]
+        });
+    }
+
+    _executeInstructions(state) {
+        const initialContext = {
+            Page: state.Page,
+            Runtime: state.Runtime,
+            instructionResults: []
+        };
+        return state.settings.instructions.reduce(
+            (promiseOfContext, instruction, index) =>
+                promiseOfContext.then(context => {
+                    this._logger.log(`> Executing instruction ${index + 1}/${state.settings.instructions.length}`);
+                    return this._executeInstruction(instruction, context).then(result =>
+                        Object.assign({}, context, {instructionResults: [...context.instructionResults, result]})
+                    );
+                }),
+            Promise.resolve(initialContext)
+        ).then(context => Object.assign({}, state, {
+            instructionContext: {instructionResults: context.instructionResults}
+        }));
+    }
+
+    _executeInstruction(instruction, context) {
+        const locations = typeof instruction.locations === 'function' ?
+            instruction.locations(context) :
+            instruction.locations;
+        const sequentialEvaluations = locations.reduce((promise, location, index) =>
+            promise.then(results => {
+                this._logger.log(`>> Executing location ${index + 1}/${locations.length}`);
+                return this._executeExpression(location, instruction.expression, context)
+                    .then(result => [...results, result])
+            }),
+            Promise.resolve([])
+        );
+        return sequentialEvaluations;
+    }
+
+    _executeExpression(location, expression, context) {
+        return new Promise((resolve, reject) => {
+            context.Page.navigate({url: location});
+            context.Page.loadEventFired(() => {
+                const params = {
+                    expression,
+                    awaitPromise: true
+                };
+                return context.Runtime.evaluate(params).then(resolve, reject);
+            });
+        })
+        .then(result => JSON.parse(this._extractResult(result)));
+    }
+
+    _extractResult(evaluationResult) {
+        if (evaluationResult.exceptionDetails) {
+            const errorString = JSON.stringify(evaluationResult.exceptionDetails, true, 2);
+            throw new Error(`Expression evaluation failed: ${errorString}`);
+        }
+        return evaluationResult.result.value;
+    }
+
+    _terminateChrome(state) {
+        state.protocol.close();
+        state.chrome.kill();
+        return state;
+    }
+
+    _buildResult(state) {
+        return state.settings.output(state.instructionContext);
+    }
+
 }
 
-function terminateChrome(state) {
-    state.protocol.close();
-    state.chrome.kill();
-    return state;
-}
-
-function buildResult(state) {
-    return state.settings.output(state.instructionContext);
-}
+module.exports = Crawler;
