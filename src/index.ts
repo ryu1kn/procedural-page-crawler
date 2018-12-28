0ebc195e52181d445af0c02880a02b7e8d89898c
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2018-12-28 21:21:30 +1100

    Rewrote promise chains with async/await

--- src/index.ts
+++ src/index.ts
@@ -29,11 +29,11 @@ interface CrawlerOption {
 }
 
 export class Crawler {
-    private readonly _logger: Logger;
+    private readonly logger: Logger;
     private readonly chromeAdaptor: ChromeAdaptor;
 
     constructor(params: CrawlerParams) {
-        this._logger = params.logger || new NullLogger();
+        this.logger = params.logger || new NullLogger();
         this.chromeAdaptor = new ChromeAdaptor();
     }
 
@@ -47,32 +47,32 @@ export class Crawler {
         }
     }
 
-    private _executeInstructions(instructions: InstructionStep[]): Promise<InstructionContext> {
+    private async _executeInstructions(instructions: InstructionStep[]): Promise<InstructionContext> {
         const initialContext = {instructionResults: []};
-        return instructions.reduce(
-            (promiseOfContext, instruction, index) =>
-                promiseOfContext.then(context => {
-                    this._logger.log(`> Executing instruction ${index + 1}/${instructions.length}`);
-                    return this._executeInstruction(instruction, context).then(result =>
-                        Object.assign({}, context, {instructionResults: [...context.instructionResults, result]})
-                    );
-                }),
+        const finalContext = await instructions.reduce(
+            async (promiseOfContext, instruction, index) => {
+                const context = await promiseOfContext;
+                this.logger.log(`> Executing instruction ${index + 1}/${instructions.length}`);
+                const result = await this._executeInstruction(instruction, context);
+                return Object.assign({}, context, {instructionResults: [...context.instructionResults, result]});
+            },
             Promise.resolve(initialContext)
-        ).then(context => ({instructionResults: context.instructionResults}));
+        );
+        return {instructionResults: finalContext.instructionResults};
     }
 
     private _executeInstruction(instruction: InstructionStep, context: InstructionContext): Promise<EvaluationResult[]> {
         const locations = typeof instruction.locations === 'function' ?
             instruction.locations(context) :
             instruction.locations;
-        const sequentialEvaluations = locations.reduce((promise, location, index) =>
-            promise.then(results => {
-                this._logger.log(`>> Executing location ${index + 1}/${locations.length}`);
-                return this.chromeAdaptor.evaluateExpression(location, instruction.expression)
-                    .then(result => [...results, result]);
-            }),
+        return locations.reduce(
+            async (promise, location, index) => {
+                const results = await promise;
+                this.logger.log(`>> Executing location ${index + 1}/${locations.length}`);
+                const result = await this.chromeAdaptor.evaluateExpression(location, instruction.expression);
+                return [...results, result];
+            },
             Promise.resolve([])
         );
-        return sequentialEvaluations;
     }
 }
